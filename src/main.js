c926b3a0e53e2da4bb89dc2821224f829b95c59a
p1aCoder <362958457@qq.com>
2020-10-11 17:16:56 +0800

    前端脚手架

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router'
+
+var axios=require('axios')
+axios.default.baseUrl='http://localhost:8443/api'
+Vue.prototype.$axios=axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
